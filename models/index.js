80751b69da61059f7e22ad75cccde2d42b518435
Sandeeep <sandeep@truelancer.com>
2021-11-26 10:09:31 +0530

    database url changes

--- models/index.js
+++ models/index.js
@@ -5,7 +5,7 @@ mongoose.Promise = Promise;
 
 
 
-mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true ,useUnifiedTopology: true});
+mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true ,useUnifiedTopology: true});
 
 
 module.exports.Blog = require('./blog');
